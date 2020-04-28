880a398a9e3a9f3e9cb42cd95f33b569ff0eafff
adrianavandeguchte <61213204+adrianavandeguchte@users.noreply.github.com>
2020-04-28 10:14:13 -0400

    Update queries.sql
    added queries 3 and 4

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -15,11 +15,22 @@ WHERE hire_date LIKE '%86';
 
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
+SELECT d.dept_id AS department_number,
+       d.dept_name AS department_name,
+       dm.emp_id AS employee_number,
+       e.last_name,
+       e.first_name
+FROM department AS d, dept_managers AS dm, employees as e
+WHERE e.emp_id=dm.emp_id AND d.dept_id=dm.dept_id;
 
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+SELECT de.emp_id AS employee_number,
+       e.last_name,
+       e.first_name,
+       d.dept_name AS department_name
+FROM department AS d, dept_emp AS de, employees as e
+WHERE e.emp_id=de.emp_id AND d.dept_id=de.dept_id;
 
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
